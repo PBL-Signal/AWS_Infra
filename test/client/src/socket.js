d195a5a6e7fb625a0f2da65cf04fc850e14a9ab4
jeongminhui99 <jeongminhee99@naver.com>
2022-07-02 16:24:54 +0900

    Update socket.js

--- test/client/src/socket.js
+++ test/client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:5000";
+const URL = "http://44.208.11.16:3000";
 const socket = io(URL, { autoConnect: false });
 
 socket.onAny((event, ...args) => {

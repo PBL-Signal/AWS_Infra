12eece46fbd2ffda17ac02de7f9156cdbab13470
jeongminhui99 <jeongminhee99@naver.com>
2022-07-09 06:28:05 +0900

    Update index.js

--- redis_test/index.js
+++ redis_test/index.js
@@ -1,52 +1,14 @@
-//require을 통해 node_modules에 있는 express를 가져올 수 있다.
-const express = require("express");
-
-//express의 반환값을 저장한다.
-const app = express();
-
 const redis = require('redis');
-const { createCluster }= require('redis');
+const REDIS_PORT = 6379
+const REDIS_URL = "redis-test.i187of.ng.0001.use1.cache.amazonaws.com"
 
-//const redis_client = redis.createClient(6379,'redis-test.i187of.ng.0001.use1.cache.amazonaws.com');
+const redis_client = redis.createClient(REDIS_PORT,REDIS_URL);
+
 // Redis
-
-const client2 = createCluster({
-  rootNodes: [
-    {
-      url: 'redis://redis-test.i187of.ng.0001.use1.cache.amazonaws.com:6379'
-    }
-  ]
+redis_client.on("error", (err) => {
+  console.error(err);
 });
 
-var client = require('redis').createClient(6379, 'redis-test.i187of.ng.0001.use1.cache.amazonaws.com', {
-  no_ready_check: true
+redis_client.on("ready", ()=> {
+  console.log("Redis is Ready");
 });
-
-client.connect().then(() => {
-  client.on("error", (err) => {
-    console.error(err);
-  });
-  
-  client.on("connect", function () {
-    console.log("connected");
-  });
-
-  
-  //check the functioning
-  client.set("framework", "AngularJS", function (err, reply) {
-    console.log("redis.set " , reply);
-  });
-
-  client.get("framework", function (err, reply) {
-    console.log("redis.get ", reply);
-  });
-});
-
-
-
-
-
-//3000번 포트로 서버를 오픈한다.
-//app.listen(3000, () => {
-  //console.log("Sever On");
-//})
